cccd54f65c907bc860e2cf1a3c7cb822f5f988aa
QQ <qq@QQs-MacBook-Pro.local>
2022-10-16 02:40:46 -0700

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.234.166.129',
+      host: '3.87.202.7',
       username: 'ubuntu',
       privateKeyPath: "labsuser.pem"
     })
